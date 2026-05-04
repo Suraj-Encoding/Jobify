03e8421bd8cf2253af3b43a5275d24b46105d485
SdDevX <suraj@leanafy.com>
2026-05-04 14:20:04 +0530

    handle clerk in react

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -8,7 +8,11 @@ import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ClerkProvider afterSignOutUrl="/sign-in">
+        <ClerkProvider
+            signInUrl="/sign-in"
+            signUpUrl="/sign-up"
+            afterSignOutUrl="/sign-in"
+        >
             <BrowserRouter>
                 <App />
             </BrowserRouter>
